92a7dae775bcbf751de5c85a3c21267f17a4604c
Shiki Nishino <105764723+Snishino93@users.noreply.github.com>
2023-05-18 01:33:08 +0000

    Empezado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,46 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    posts = relationship('Post', backref='user', lazy=True)# Relación uno a muchos con la clase Post
+    comments = relationship('Comment', backref='user', lazy=True) # Relación uno a muchos con la clase Comment
+    likes = relationship('Like', backref='user', lazy=True) # Relación uno a muchos con la clase Like
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    image_url = Column(String(250), nullable=False)
+    caption = Column(String(1000), nullable=True)
+    created_at = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False) # FK de user
+    comments = relationship('Comment', backref='post', lazy=True) # Relación uno a muchos con la clase Comment
+    likes = relationship('Like', backref='post', lazy=True) # Relación uno a muchos con la clase Like
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(1000), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)  
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False) 
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
 
     def to_dict(self):
         return {}
